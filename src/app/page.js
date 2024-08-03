f691c4da7ea1c87a9935e91871b2136031312596
gayan <gayanashoka38@gmail.com>
2024-08-03 20:52:05 +0530

    create navbar component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import { Container } from "postcss";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]  ">
+      <Navbar/>
      <div className="container mx-auto py-4 px-12">
+      
       <HeroSection/>
     </div>
     </main>
